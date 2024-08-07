ed6fe99f62ac1330cc804fba329b76ed251b8f94
Markus Seeli <markus@seeli.eu>
2024-08-07 11:31:20 +0000

    added database tools

---
+++ fabric.session.sql
@@ -0,0 +1,7 @@
+-- UPDATE models SET name = 'gpt-4o-2024-08-06' WHERE id = 1;
+/*SELECT *
+ FROM models
+ 
+ WHERE id = 1;   */
+select *
+from users;
--- hash.py
+++ hash.py
@@ -1,7 +1,8 @@
 import hashlib
 
+
 def hash_string(string):
     return hashlib.sha256(string.encode()).hexdigest()
 
 
-print(hash_string("hello"))
+print(hash_string("miau"))
